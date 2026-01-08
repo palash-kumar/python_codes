4b627cb4d16b673cb0422ee108c3390c8c0d2ff1
palash <prince_nath@hotmail.com>
2026-01-08 15:49:46 +0600

    7_Functions_Lambdas_and_List_Comprehensions.py

--- 7_Functions_Lambdas_and_List_Comprehensions.py
+++ 7_Functions_Lambdas_and_List_Comprehensions.py
@@ -15,6 +15,11 @@ discount = float(input("Enter discount percentage (0-100): "))
 discounted_prices = dicount_prices(prices, discount)
 print(discounted_prices)
 
+
+'''Problem 2: The "Short Word" Filter
+The Challenge: You have a list of words. Some are long, some are short.Create a Lambda function that checks if a word has more than 3 letters (returns True or False).
+Use a List Comprehension and that Lambda to create a new list containing only the "Long Words."
+'''
 words = ["hi", "python", "is", "cool", "code", "a"]
 
 is_long = lambda word: len(word) > 3
